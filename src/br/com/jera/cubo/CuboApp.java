8aa35602570918a184c0fde1558235b6ac156373
Andre Santee <andre.santee@gmail.com>
2011-02-05 12:55:50 -0300

    Testando sprites

--- src/br/com/jera/cubo/CuboApp.java
+++ src/br/com/jera/cubo/CuboApp.java
@@ -1,11 +1,13 @@
 ﻿package br.com.jera.cubo;
 
 import br.com.jera.graphic.GraphicDevice;
+import br.com.jera.graphic.GraphicDevice.ALPHA_MODE;
 import br.com.jera.graphic.Math.PRIMITIVE_TYPE;
 import br.com.jera.graphic.Math.Vector2;
 import br.com.jera.graphic.Math.Vector3;
 import br.com.jera.graphic.Math.Vector4;
 import br.com.jera.graphic.Math.Vertex;
+import br.com.jera.graphic.Sprite;
 import br.com.jera.graphic.Texture;
 import br.com.jera.graphic.VertexArray;
 
@@ -15,15 +17,26 @@ public class CuboApp {
 	private VertexArray cube;
 	private float angle = 0.0f;
 	private Texture jeraTexture;
-	
+	private Sprite jeraSprite;
+
 	public void draw() {
 
-		angle += 0.9f;
+		angle += 1.0f;
 		graphicDevice.beginScene();
 		jeraTexture.bindTexture();
-		cube.drawGeometry(new Vector3(0, 0, -7),
+
+		graphicDevice.setup3DView();
+		graphicDevice.setAlphaMode(ALPHA_MODE.NONE);
+		cube.drawGeometry(new Vector3(0, 0, 5),
 				new Vector3(0, angle, 0), new Vector3(1, 1, 1));
 		jeraTexture.unbindTexture();
+
+		graphicDevice.setup2DView();
+		graphicDevice.setAlphaMode(ALPHA_MODE.DEFAULT);
+		jeraSprite.draw(new Vector2(10,10).add(graphicDevice.getScreenSize().multiply(-0.5f)),
+				jeraSprite.getBitmapSize(),
+				0, new Vector2(0.0f, 0.0f), 0);
+
 		graphicDevice.endScene();
 	}
 
@@ -33,43 +46,44 @@ public class CuboApp {
 		graphicDevice.setCullingMode(GraphicDevice.CULLING_MODE.CULL_NONE);
 		Vertex[] vertices = {
 				// face 1
-				new Vertex(new Vector3(-1, 1, -1), new Vector2(0, 0)),
-				new Vertex(new Vector3(-1, -1, -1), new Vector2(0, 1)),
-				new Vertex(new Vector3(1, -1, -1), new Vector2(1, 1)),
+				new Vertex(new Vector3(-1, 1, 1), new Vector2(0, 0)),
+				new Vertex(new Vector3(-1, -1, 1), new Vector2(0, 1)),
+				new Vertex(new Vector3(1, -1, 1), new Vector2(1, 1)),
 
-				new Vertex(new Vector3(1, 1, -1), new Vector2(1, 0)),
-				new Vertex(new Vector3(-1, 1, -1), new Vector2(0, 0)),
-				new Vertex(new Vector3(1, -1, -1), new Vector2(1, 1)),
+				new Vertex(new Vector3(1, 1, 1), new Vector2(1, 0)),
+				new Vertex(new Vector3(-1, 1, 1), new Vector2(0, 0)),
+				new Vertex(new Vector3(1, -1, 1), new Vector2(1, 1)),
 
 				// face 2
-				new Vertex(new Vector3(1, -1, 1), new Vector2(1, 1)),
-				new Vertex(new Vector3(-1, -1, 1), new Vector2(0, 1)),
-				new Vertex(new Vector3(-1, 1, 1), new Vector2(0, 0)),
+				new Vertex(new Vector3(1, -1, -1), new Vector2(1, 1)),
+				new Vertex(new Vector3(-1, -1, -1), new Vector2(0, 1)),
+				new Vertex(new Vector3(-1, 1, -1), new Vector2(0, 0)),
 
-				new Vertex(new Vector3(-1, 1, 1), new Vector2(0, 0)),
-				new Vertex(new Vector3(1, 1, 1), new Vector2(1, 0)),
-				new Vertex(new Vector3(1, -1, 1), new Vector2(1, 1)),
+				new Vertex(new Vector3(-1, 1, -1), new Vector2(0, 0)),
+				new Vertex(new Vector3(1, 1, -1), new Vector2(1, 0)),
+				new Vertex(new Vector3(1, -1, -1), new Vector2(1, 1)),
 
 				// face 3
-				new Vertex(new Vector3(-1, -1, 1), new Vector2(1, 1)),
-				new Vertex(new Vector3(-1, -1, -1), new Vector2(1, 0)),
-				new Vertex(new Vector3(-1, 1, -1), new Vector2(0, 0)),
+				new Vertex(new Vector3(-1, -1, -1), new Vector2(1, 1)),
+				new Vertex(new Vector3(-1, -1, 1), new Vector2(1, 0)),
+				new Vertex(new Vector3(-1, 1, 1), new Vector2(0, 0)),
 
-				new Vertex(new Vector3(-1, -1, 1), new Vector2(1, 1)),
-				new Vertex(new Vector3(-1, 1, -1), new Vector2(0, 0)),
-				new Vertex(new Vector3(-1, 1, 1), new Vector2(0, 1)),
+				new Vertex(new Vector3(-1, -1, -1), new Vector2(1, 1)),
+				new Vertex(new Vector3(-1, 1, 1), new Vector2(0, 0)),
+				new Vertex(new Vector3(-1, 1, -1), new Vector2(0, 1)),
 
 				// face 4
-				new Vertex(new Vector3(1, -1, -1), new Vector2(1, 0)),
-				new Vertex(new Vector3(1, -1, 1), new Vector2(1, 1)),
-				new Vertex(new Vector3(1, 1, -1), new Vector2(0, 0)),
+				new Vertex(new Vector3(1, -1, 1), new Vector2(1, 0)),
+				new Vertex(new Vector3(1, -1, -1), new Vector2(1, 1)),
+				new Vertex(new Vector3(1, 1, 1), new Vector2(0, 0)),
 
-				new Vertex(new Vector3(1, -1, 1), new Vector2(1, 1)),
-				new Vertex(new Vector3(1, 1, 1), new Vector2(0, 1)),
-				new Vertex(new Vector3(1, 1, -1), new Vector2(0, 0)),
+				new Vertex(new Vector3(1, -1, -1), new Vector2(1, 1)),
+				new Vertex(new Vector3(1, 1, -1), new Vector2(0, 1)),
+				new Vertex(new Vector3(1, 1, 1), new Vector2(0, 0)),
 			};
 
 		cube = graphicDevice.createVertexArray(vertices, PRIMITIVE_TYPE.TRIANGLE_LIST);
 		jeraTexture = graphicDevice.createStaticTexture(R.drawable.jera);
+		jeraSprite = new Sprite(graphicDevice, R.drawable.logo_jera, 1, 1);
 	}
 }
